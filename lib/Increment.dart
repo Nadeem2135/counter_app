3016cf1ec78af2f5d55a0302ce27e8e12bdebb03
NADEEM <nsarwar2135@gmail.com>
2026-05-08 22:44:58 +0500

    Changes Done in test 2

--- lib/Increment.dart
+++ lib/Increment.dart
@@ -15,6 +15,8 @@ class _IncrementState extends State<Increment> {
       body: ListView.builder(itemBuilder: (context, index){
         return ListTile(
           title: Text("Flutter Dev"),
+          subtitle: Text("Flutter Dev"),
+          trailing: Icon(Icons.add_a_photo),
         );
       }),
     );
